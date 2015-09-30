d5069b517538fbc61c1b720a261ac1a75531336b
Tanmay Bakshi <tajymany@gmail.com>
2015-09-30 00:59:34 -0400

    Initial Commit

---
+++ YouTubeSpeechTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouTubeSpeechTest
+//
+//  Created by Tanmay Bakshi on 2015-09-30.
+//  Copyright © 2015 Tanmay Bakshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
